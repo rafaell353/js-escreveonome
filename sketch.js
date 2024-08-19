ee40ef6f82af1aac61f05b0c8fc0de4a188d4ea5
rafael <eduardo.lemes.rafael@escola.pr.gov.br>
2024-08-19 20:33:49 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,1 +1,12 @@
-
+function setup() {
+  createCanvas(600, 400);
+ background("red");
+}
+function draw() {
+ 
+  stroke("purple");
+  fill("black");
+  if (mouseIsPressed) {
+    rect(mouseX, mouseY, 20, 35);
+  }
+}
